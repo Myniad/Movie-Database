29cbe639c6344847e719b31fb1b0ac7f5c4d9a7a
Junyoung Kim <jkim5929@gmail.com>
2023-01-24 18:33:31 -0500

    First Commit

---
+++ Movie.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie_Database
+{
+    public class Movie
+    {
+        //properties
+        public string Title { get; set; }
+        public string Category { get; set; }
+
+        //constructor
+        public Movie(string _title, string _category)
+        {
+            Title = _title;
+            Category = _category;
+        }
+        public string GetDetails()
+        {
+            return $"-{Title}";
+        }
+
+
+        public static bool GetContinue(string msg)
+        {
+            bool result = true;
+
+            while (true)
+            {
+                Console.WriteLine($"{msg} y/n");
+                string choice = Console.ReadLine().Trim().ToLower();
+                if (choice == "y")
+                {
+                    result = true;
+                    break;
+                }
+                else if (choice == "n")
+                {
+                    result = false;
+                    Console.WriteLine("Goodbye.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Try again.");
+                }
+            }
+            return result;
+        }
+
+        //public static bool GetSelection()
+        //{
+        //    while (true)
+        //    {
+
+        //    }
+        //}
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Movie_Database;
+
+bool runProgram = true;
+string category = "";
+
+while (runProgram)
+{
+    List<Movie> movie = new List<Movie>()
+{
+    //categories: animated, drama, horror, scifi
+    new Movie("Stargate","scifi"),
+    new Movie("Castle in the Sky", "animated"),
+    new Movie("The Red Shoes", "horror"),
+    new Movie("Titanic", "drama"),
+    new Movie("Edge of Tomorrow", "scifi"),
+    new Movie("Princess Mononoke", "animated"),
+    new Movie("The Thing", "horror"),
+    new Movie("Logan", "drama"),
+    new Movie("Nausicaa of the Valley of the Wind", "animated"),
+    new Movie("To KIll a Mockingbird", "drama"),
+    new Movie("Tron: Legacy", "scifi"),
+    new Movie("28 Days Later", "Horror")
+};
+    //----------------------------------------------------------------------------------------------------------------------------------------------------
+
+    Console.WriteLine("Welcome to the Movie List Application!");
+    Console.WriteLine($"There are {movie.Count} movies in this list. What category are you interested in? ");
+    while (true)
+    {
+        
+            Console.WriteLine("We have scifi, animated, horror, and drama ");
+            category = Console.ReadLine().ToLower().Trim();
+        Console.WriteLine("------------");
+        if (category == "scifi"||category=="animated"||category=="horror"||category=="drama")
+        {
+            foreach (Movie m in movie.Where(s => s.Category.ToLower() == category))
+            {
+                Console.WriteLine(m.GetDetails());
+            }
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid category.");
+        }
+    }
+    Console.WriteLine("------------");
+        runProgram = Movie.GetContinue("\nContinue?");
+
+}
+
